bc85446ff783a7af0b28e91295ae0f9a67dfbe9c
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-18 20:12:46 +0200

    commit

--- src/main/java/com/tpe/domain/Hotel.java
+++ src/main/java/com/tpe/domain/Hotel.java
@@ -17,4 +17,51 @@ public class Hotel {
     private String location;
     @OneToMany(mappedBy = "hotel")  // private Hotel hotel; -> in the room class
     private List<Room> rooms = new ArrayList<>();
+
+    public Hotel() {
+    }
+
+    public Hotel(Long id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", rooms=" + rooms +    // if we want to, we can change this to rooms.size()
+                '}';
+    }
 }
