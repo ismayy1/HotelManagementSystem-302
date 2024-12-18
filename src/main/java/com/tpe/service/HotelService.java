eea8e2164302bc024ce9da90f263e4c3e19c8f83
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-12-18 20:53:05 +0200

    commit

--- src/main/java/com/tpe/service/HotelService.java
+++ src/main/java/com/tpe/service/HotelService.java
@@ -1,5 +1,6 @@
 package com.tpe.service;
 
+import com.tpe.domain.Hotel;
 import com.tpe.repository.HotelRepository;
 
 import java.util.Scanner;
@@ -17,5 +18,15 @@ public class HotelService {
     //    TASK 1-c:
     public void saveHotel() {
 
+        Hotel newHotel = new Hotel();
+
+        System.out.println("Enter Hotel Name:");
+        newHotel.setName(scanner.nextLine());
+
+        System.out.println("Enter Hotel Location:");
+        newHotel.setLocation(scanner.nextLine());
+
+        hotelRepository.save(newHotel);
+        System.out.println("The Hotel is saved successfully!");
     }
 }
